00f03b4eb58b1038fbe488c6ffdcfe578e61ca3b
AnnoyingRains <AnnoyingRain5@gmail.com>
2022-09-28 22:14:47 +1000

    Migrated Translator to DatabaseManager

--- cogs/Translator.py
+++ cogs/Translator.py
@@ -5,6 +5,7 @@ import googletrans
 import json
 from discord.commands import SlashCommandGroup
 from discord.ext import commands
+from lib.DatabaseManager import Database
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,17 +18,16 @@ class Translator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-        with open('db/Translator.json', 'r') as f:
-            self.database = json.loads(f.read())
+        self.db = Database("Translator")
             
     TranslatorSlashGroup = SlashCommandGroup("translator", "Configure the translator")
     TranslatorChannelSlashGroup = TranslatorSlashGroup.create_subgroup("channelgroup", "Configure Channel Groups")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        if guild.id not in self.database: # if the server does not have any ChannelGroups
+        if guild.id not in self.db.read(): # if the server does not have any ChannelGroups
             # generate empty template
-            self.database.update(
+            self.db.update(
                 {
                     guild.id: {
                         "ChannelGroups": {"0", {"": ""}},
@@ -35,18 +35,14 @@ class Translator(commands.Cog):
                     }
                 }
             )
-        with open('db/Translator.json', 'w') as f:
-            f.write(json.dumps(self.database, indent=4))
         
     @commands.Cog.listener()
     async def on_ready(self):
-        with open('db/Translator.json', 'r') as f:
-            self.database = json.loads(f.read())
         for guild in self.bot.guilds:
             # if the server does not have any ChannelGroups
-            if str(guild.id) not in self.database:
+            if str(guild.id) not in self.db.read():
                 # generate empty template
-                self.database.update(
+                self.db.update(
                     {
                         guild.id: {
                             "ChannelGroups": {"0": {"": ""}},
@@ -54,21 +50,16 @@ class Translator(commands.Cog):
                         }
                     }
                 )
-                with open('db/Translator.json', 'w') as f:
-                    f.write(json.dumps(self.database, indent=4))
-                # I have no idea why this works...
-                with open('db/Translator.json', 'r') as f:
-                    self.database = json.loads(f.read())
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if ctx.webhook_id == None: # if the message isnt from a webhook
-            for ChannelGroupID in self.database[str(ctx.guild.id)]["ChannelGroups"]:
-                ChannelGroup = self.database[str(ctx.guild.id)]["ChannelGroups"][ChannelGroupID]
+            for ChannelGroupID in self.db.read()[str(ctx.guild.id)]["ChannelGroups"]:
+                ChannelGroup = self.db.read()[str(ctx.guild.id)]["ChannelGroups"][ChannelGroupID]
                 if str(ctx.channel.id) in ChannelGroup: # if the channel is in the channelGroup
                     for channel in ChannelGroup:
                         if channel != str(ctx.channel.id) and channel != "0": # dont send the message in its own channel, and dont send it to ID 0
-                            webhook = Webhook.from_url(self.database[str(ctx.guild.id)]["webhooks"][str(channel)], session=self.session)
+                            webhook = Webhook.from_url(self.db.read()[str(ctx.guild.id)]["webhooks"][str(channel)], session=self.session)
                             SourceLang = ChannelGroup[str(ctx.channel.id)] # language of the channel the original message was in
                             DestinationLang = ChannelGroup[str(channel)] # language of the current channel in the ChannelGroup
                             TranslatedText = translator.translate(ctx.content, DestinationLang, SourceLang).text
@@ -82,25 +73,23 @@ class Translator(commands.Cog):
                  
         ChannelGroup = {channel1: channel1language, channel2: channel2language, channel3: channel3language, channel4: channel4language, channel5: channel5language}
         WebhookTable = {channel1: channel1webhook, channel2: channel2webhook, channel3: channel3webhook, channel4: channel4webhook, channel5: channel5webhook}
-        ChannelGroupID = str(int(list(self.database[str(ctx.guild.id)]["ChannelGroups"])[-1])+1)
-        self.database[str(ctx.guild.id)]["ChannelGroups"].update({ChannelGroupID: ChannelGroup}) # ensure each channelgroup is seperate
-        self.database[str(ctx.guild.id)]["webhooks"].update(WebhookTable)
-        with open('db/Translator.json', 'w') as f:
-            f.write(json.dumps(self.database, indent=4))
+        ChannelGroupID = str(int(list(self.db.read()[str(ctx.guild.id)]["ChannelGroups"])[-1])+1)
+        self.db.db[str(ctx.guild.id)]["ChannelGroups"].update({ChannelGroupID: ChannelGroup}) # ensure each channelgroup is seperate
+        self.db.db[str(ctx.guild.id)]["webhooks"].update(WebhookTable)
+        self.db.save()
         await ctx.respond("Channel group created!")
 
     @TranslatorSlashGroup.command(description="Display the server config JSON")
     async def viewserverconfig(self, ctx):
-        await ctx.respond(f"```json\n\n{json.dumps(self.database[str(ctx.guild.id)], indent=4)}\n```")
+        await ctx.respond(f"```json\n\n{json.dumps(self.db.read()[str(ctx.guild.id)], indent=4)}\n```")
         
     @TranslatorChannelSlashGroup.command(description="Remove a channel group")
     async def remove(self, ctx, channelgroupid: int):
         if channelgroupid == 0:
             await ctx.respond("You cannot remove group ID 0! That group is reserved to ensure the bot doesn't crash...")
         else:
-            self.database[str(ctx.guild.id)]["ChannelGroups"].pop(str(channelgroupid))
-            with open('db/Translator.json', 'w') as f:
-                f.write(json.dumps(self.database, indent=4))
-                
+            self.db.db[str(ctx.guild.id)]["ChannelGroups"].pop(str(channelgroupid))
+            self.db.save()
+            await ctx.respond("Done!")
 def setup(bot):
     bot.add_cog(Translator(bot))
